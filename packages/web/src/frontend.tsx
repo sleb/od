d93f1ac683dfdad24d084c6595419d8d143dc6d2
sleb <scott.g.lebaron@gmail.com>
2025-12-27 22:41:53 -0500

    feat(web): add react-hook-form and react-router-dom dependencies; update frontend entry point to use RouterProvider

--- packages/web/src/frontend.tsx
+++ packages/web/src/frontend.tsx
@@ -1,17 +1,18 @@
-/**
- * This file is the entry point for the React app, it sets up the root
- * element and renders the App component to the DOM.
- *
- * It is included in `src/index.html`.
- */
-
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App";
 
-function start() {
+const router = createBrowserRouter([{ path: "/*", element: <App /> }]);
+
+const start = () => {
   const root = createRoot(document.getElementById("root")!);
-  root.render(<App />);
-}
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  );
+};
 
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", start);
