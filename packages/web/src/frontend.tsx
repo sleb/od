5b39e5854c4f24458ab52561a41661ba980dc479
sleb <scott.g.lebaron@gmail.com>
2026-01-08 19:34:09 -0800

    Refactor import statements to use absolute paths in frontend and router files

--- packages/web/src/frontend.tsx
+++ packages/web/src/frontend.tsx
@@ -1,9 +1,9 @@
+import Loading from "@/components/loading";
 import { router } from "@/router";
 import { MantineProvider } from "@mantine/core";
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import Loading from "./components/loading";
 
 const start = async () => {
   const root = createRoot(document.getElementById("root")!);
--- packages/web/src/router.tsx
+++ packages/web/src/router.tsx
@@ -1,12 +1,12 @@
+import AuthRequired from "@/components/auth-required";
+import DeviceDetailPage from "@/components/devices/device-detail-page";
+import DevicesListPage from "@/components/devices/devices-list-page";
+import HomePage from "@/components/home/home-page";
+import LandingPage from "@/components/landing/landing-page";
+import AppShell from "@/components/layout/app-shell";
+import LoginPage from "@/components/login/login-page";
+import SignUpPage from "@/components/signup/signup-page";
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import AuthRequired from "./components/auth-required";
-import DeviceDetailPage from "./components/devices/device-detail-page";
-import DevicesListPage from "./components/devices/devices-list-page";
-import HomePage from "./components/home/home-page";
-import LandingPage from "./components/landing/landing-page";
-import AppShell from "./components/layout/app-shell";
-import LoginPage from "./components/login/login-page";
-import SignUpPage from "./components/signup/signup-page";
 
 export const router = createBrowserRouter([
   {
